483787b15ac2351575cac95fc28eec08a3eabd0c
SubJunk <subjunk@gmail.com>
2018-07-28 12:18:29 +1200

    again

--- app/app.js
+++ app/app.js
@@ -979,7 +979,7 @@ angular.module('app', ['angular-md5'])
     * the garbage collector, if the relevant GET parameters
     * are specified (id and gc)
     */
-    var useGetParameters = function() {
+    var useGetParameters = functi on() {
       if ($location.search()) { 
         var searchParams = $location.search();
 
