6c0bfbc56be1534efd48d00d26c3f7d9f38cfa0d
Paulene Crook <pg.girl@gmail.com>
2018-12-11 11:48:11 +1300

    added first year label test

--- test/specs/labels.spec.js
+++ test/specs/labels.spec.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+
 describe('Labels', function() {
   before(function() {
     this.timeout(200000);
@@ -8,13 +10,13 @@ describe('Labels', function() {
     browser.waitForVisible('#label-0*=Uncanny X-Men');
   });
 
-  // it('should create a first year label with the correct value', function() {
-  //   //not sure why this doesn't work
-  //   var assert = require('assert');
-  //   browser.waitForVisible('ul.years li:nth-child(1)');
-  //   var firstYearText = browser.getText('ul.years li:nth-child(1)');
-  //   assert.equal(firstYearText, '1975');
-  // });
+  it('should create a first year label with the correct value', function() {
+    browser.waitForVisible('ul.years > li:nth-child(1)');
+    var firstYearText = browser.getText('ul.years > li:nth-child(1)');
+    firstYearText = String(firstYearText);
+    firstYearText = firstYearText.substring(0,4);
+    assert.equal(firstYearText, '1975');
+  });
 
   it('should create a first month label with the correct value', function() {
     var assert = require('assert');
