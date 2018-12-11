95c851c1b2da84efad659406e71db6a92282b783
Paulene Crook <pg.girl@gmail.com>
2018-12-11 15:46:50 +1300

    WIP changing selectors test on Travis

--- test/specs/comics.spec.js
+++ test/specs/comics.spec.js
@@ -6,14 +6,14 @@ describe('Comics', function() {
     });
   
    it('should create a comic thumbnail with the correct value', function() {
-    browser.waitForVisible('ul.comics li:nth-child(172) div.comic.z-depth-5.hoverable div img');
-    var firstThumbSrc = browser.getAttribute('ul.comics li:nth-child(172) div.comic.z-depth-5.hoverable div img', 'src');
+    browser.waitForVisible('ul.comics > li:nth-child(172) > div.comic.z-depth-5.hoverable > div > a > img');
+    var firstThumbSrc = browser.getAttribute('ul.comics > li:nth-child(172) > div.comic.z-depth-5.hoverable > div > a > img', 'src');
     assert.equal(firstThumbSrc, 'https://www.spirton.com/images/comics/Giant_Size_X-Men_Vol_1_1.jpg');
    });
 
    it ('should open the expanded panel with the correct title', function() {
-    browser.waitForVisible('ul.comics li:nth-child(172) div.comic.z-depth-5.hoverable div img');
-    browser.click('ul.comics li:nth-child(172) div.comic.z-depth-5.hoverable div a');
+    browser.waitForVisible('ul.comics > li:nth-child(172) div.comic.z-depth-5.hoverable div img');
+    browser.click('ul.comics > li:nth-child(172) div.comic.z-depth-5.hoverable div a');
     browser.waitForVisible('div.series*=Giant Size X-Men #1');
   });
 });
--- test/specs/labels.spec.js
+++ test/specs/labels.spec.js
@@ -6,11 +6,11 @@ describe('Labels', function() {
     browser.url('file://' + process.cwd() + '/index.html');
   });
 
-  it('should create a label with the correct value', function() {
+  it('should create a series label with the correct value', function() {
     browser.waitForVisible('#label-0*=Uncanny X-Men');
   });
 
-  it('should create a first year label with the correct value', function() {
+  it('should create a first year heading with the correct value', function() {
     browser.waitForVisible('ul.years > li:nth-child(1)');
     var firstYearText = browser.getText('ul.years > li:nth-child(1)');
     firstYearText = String(firstYearText);
@@ -18,7 +18,7 @@ describe('Labels', function() {
     assert.equal(firstYearText, '1975');
   });
 
-  it('should create a first month label with the correct value', function() {
+  it('should create a first month heading with the correct value', function() {
     var assert = require('assert');
     browser.waitForVisible('ul.years li:nth-child(1) ul li:nth-child(1)');
     var firstMonthText = browser.getText('ul.years li:nth-child(1) ul li:nth-child(1)');
