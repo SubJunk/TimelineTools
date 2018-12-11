3a0a994dd2ca492fa8c68daf892eb0f2789e34e8
Paulene Crook <pg.girl@gmail.com>
2018-12-11 21:19:09 +1300

    logging in Travis

--- test/specs/comics.spec.js
+++ test/specs/comics.spec.js
@@ -3,11 +3,15 @@ var assert = require('assert');
 describe('Comics', function() {
    before(function() {
       browser.url('file://' + process.cwd() + '/index.html');
+      console.log('before' + browser.url);
     });
   
    it('should create a comic thumbnail with the correct value', function() {
+       console.log('in the it statement');
     browser.waitForVisible('ul.comics > li:nth-child(172) > div.comic.z-depth-5.hoverable > div > a > img');
+    console.log('wait for visible passed')
     var firstThumbSrc = browser.getAttribute('ul.comics > li:nth-child(172) > div.comic.z-depth-5.hoverable > div > a > img', 'src');
+    console.log('first thumb src' + firstThumbSrc);
     assert.equal(firstThumbSrc, 'https://www.spirton.com/images/comics/Giant_Size_X-Men_Vol_1_1.jpg');
    });
 
