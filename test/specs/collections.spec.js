3ff58f8decc4044be0fb85f7bd92f08155b9f12c
Paulene Crook <pg.girl@gmail.com>
2018-12-12 15:15:07 +1300

    remove artefacts re-enable collections

--- test/specs/collections.spec.js
+++ test/specs/collections.spec.js
@@ -1,13 +1,13 @@
-// var assert = require('assert');
+var assert = require('assert');
 
-// describe('Collections', function() {
-//     before(function() {
-//         browser.url('file://' + process.cwd() + '/index.html#!?showCollections=1');
-//     });
+describe('Collections', function() {
+    before(function() {
+        browser.url('file://' + process.cwd() + '/index.html#!?showCollections=1');
+    });
 
-//     it('should create a collection image with the correct value', function() {
-//      browser.waitForVisible('ul.collections > li:nth-child(1) div > div.panel-left');
-//      var firstCollectionSrc = browser.getAttribute('ul.collections li:nth-child(1) div div.panel-left a img', 'src');
-//      assert.equal(firstCollectionSrc, 'https://www.spirton.com/images/comics/X-Men_Epic_Collection_Vol_5_Second_Genesis.jpg');
-//     });
-// });
+    it('should create a collection image with the correct value', function() {
+     browser.waitForVisible('ul.collections > li:nth-child(1) div > div.panel-left');
+     var firstCollectionSrc = browser.getAttribute('ul.collections li:nth-child(1) div div.panel-left a img', 'src');
+     assert.equal(firstCollectionSrc, 'https://www.spirton.com/images/comics/X-Men_Epic_Collection_Vol_5_Second_Genesis.jpg');
+    });
+});
