8d9c4322be7a41d7a9ae13d1b469057d304db7ab
SubJunk <subjunk@gmail.com>
2018-12-16 21:24:57 +1300

    Formatting

--- test/specs/labels.spec.js
+++ test/specs/labels.spec.js
@@ -2,7 +2,6 @@ var assert = require('assert');
 
 describe('Labels', function() {
   before(function() {
-    this.timeout(200000);
     browser.url('file://' + process.cwd() + '/index.html');
   });
 
@@ -13,9 +12,7 @@ describe('Labels', function() {
   it('should create a first year heading with the correct value', function() {
     browser.waitForVisible('ul.years > li:nth-child(1)');
     var firstYearText = browser.getText('ul.years > li:nth-child(1)');
-    firstYearText = String(firstYearText);
-    firstYearText = firstYearText.substring(0,4);
-    assert.equal(firstYearText, '1975');
+    assert(firstYearText.indexOf('1975') > -1);
   });
 
   it('should create a first month heading with the correct value', function() {
