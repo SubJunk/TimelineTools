7cdb18190aeb2bc2692ba0725df9c934e33790d1
SubJunk <subjunk@gmail.com>
2018-12-16 20:50:06 +1300

    Fixed tests

--- test/specs/comics.spec.js
+++ test/specs/comics.spec.js
@@ -1,24 +1,17 @@
 var assert = require('assert');
 
 describe('Comics', function() {
-   before(function() {
-      browser.url('file://' + process.cwd() + '/index.html');
-      console.log('before' + browser.getUrl());
-    });
+  before(function() {
+    browser.url('file://' + process.cwd() + '/index.html');
+  });
   
-   it('should create a comic thumbnail with the correct value', function() {
-   console.log('in the it statement');
-//    'ul.comics > li:nth-child(172) > div.comic.z-depth-5.hoverable > div > a > img'
-    browser.waitForVisible('ul.comics > li:nth-child(172) > div.comic.z-depth-5.hoverable > div > a > img[src*=Giant]');
-    // console.log('wait for visible passed')
-    // firstThumbSrc = browser.getAttribute('ul.comics > li:nth-child(172) > div.comic.z-depth-5.hoverable > div > a > img', 'src');
-    // console.log('first thumb src' + firstThumbSrc);
-    // assert.equal(firstThumbSrc, 'https://www.spirton.com/images/comics/Giant_Size_X-Men_Vol_1_1.jpg');
-   });
+  it('should create a comic thumbnail with the correct value', function() {
+    browser.waitForVisible('.cover-thumbnail img[src*=Giant_Size_X-Men_Vol_1_1]');
+  });
 
-   it ('should open the expanded panel with the correct title', function() {
-    browser.waitForVisible('ul.comics > li:nth-child(172) div.comic.z-depth-5.hoverable div img');
-    browser.click('ul.comics > li:nth-child(172) div.comic.z-depth-5.hoverable div a');
+  it('should open the expanded panel with the correct title', function() {
+    browser.waitForVisible('.cover-thumbnail img[src*=Giant_Size_X-Men_Vol_1_1]');
+    browser.click('.cover-thumbnail img[src*=Giant_Size_X-Men_Vol_1_1]');
     browser.waitForVisible('div.series*=Giant Size X-Men #1');
   });
 });
