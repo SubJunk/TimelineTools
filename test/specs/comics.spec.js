c71c2f6ba1cc77d8df576af9c65f7ac5cf6511c9
SubJunk <SubJunk@users.noreply.github.com>
2019-01-15 16:51:02 +1300

    Display a loading state while Angular starts up (#76)
    * Display a loading state while Angular starts up
    
    * Fixed pagination and minimum box size

--- test/specs/comics.spec.js
+++ test/specs/comics.spec.js
@@ -3,7 +3,11 @@ describe('Comics', function() {
     browser.url('file://' + process.cwd() + '/index.html');
   });
   
-  it('should create a comic thumbnail with the correct value, and open the expanded panel with the correct title', function() {
+  it.only(
+    'should create a comic thumbnail with the correct value\n' +
+    'should open the expanded panel with the correct title\n' +
+    'should use the arrow button to go to the next comic',
+  function() {
     const giantSizeThumbnail = $('.cover-thumbnail img[src*=Uncanny_X-Men_Vol_1_2]');
     giantSizeThumbnail.waitForDisplayed();
 
@@ -12,5 +16,12 @@ describe('Comics', function() {
 
     const giantSizeExpandedPanelTitle = $('div.series*=Uncanny X-Men #2');
     giantSizeExpandedPanelTitle.waitForDisplayed();
+
+    const nextComicBtn = $('.button-next-comic');
+    nextComicBtn.waitForDisplayed();
+    nextComicBtn.click();
+
+    const nextExpandedPanelTitle = $('div.series*=Uncanny X-Men #3');
+    nextExpandedPanelTitle.waitForDisplayed();
   });
 });
