f0d93e76eeef485af65f707866c32d6cf73d8fcb
SubJunk <subjunk@gmail.com>
2019-04-09 10:06:01 +1200

    test

--- test/specs/comics.spec.js
+++ test/specs/comics.spec.js
@@ -8,11 +8,12 @@ describe('Comics', function() {
     'should open the expanded panel with the correct title\n' +
     'should use the arrow button to go to the next comic',
   function() {
+    const preloadedThumbnails = $('#pre-app');
+    // This confusing syntax means wait for it to NOT be displayed
+    preloadedThumbnails.waitForDisplayed(500, true);
+
     const giantSizeThumbnail = $('.cover-thumbnail img[src*=Uncanny_X-Men_Vol_1_5]');
     giantSizeThumbnail.waitForDisplayed();
-
-    const preloadedThumbnails = $('#pre-app');
-    preloadedThumbnails.waitForDisplayed(500, true);
 
     const giantSizeExpandLink = $('#expand-UncannyXMenVol15');
     giantSizeExpandLink.click();
