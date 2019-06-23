f0c795ed3df6b37d6dff9cf2de237fc147e74a83
SubJunk <subjunk@gmail.com>
2019-06-23 20:13:06 +1200

    fixups

--- e2e/src/collections.e2e-spec.ts
+++ e2e/src/collections.e2e-spec.ts
@@ -1,31 +1,30 @@
 import { AppPage } from './app.po';
-import { browser, by, element, protractor, logging } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 describe('Collections', () => {
     let page: AppPage;
-  
+
     beforeEach(() => {
       page = new AppPage();
-      //somehow need to update this to whatever should actually be appended to show collections
-      browser.getCurrentUrl().then(function (url) {
-        browser.get(url + "?showCollections=1");
-        });
-    });
-
-    it('should create a collection image', async () => {
-        expect
-        //collections aren't working at the moment, change to .collections when they are
-        await element(by.css('.responsive-img')).isDisplayed()
+      // somehow need to update this to whatever should actually be appended to show collections
+      browser.getCurrentUrl().then((url) => {
+        browser.get(url + '?showCollections=1');
       });
-
-    it ('should display the correct collection image'), async () => {
-        // const firstCollectionSrc = element(by.css("img[src*=Uncanny]"));
-        // expect(firstCollectionSrc.isPresent()).toBe(true); 
-        expect(
-          await element(by.classname('.responsive-img')).getAttribute('src')
-        ).toContain(
-          'Uncanny'
-        );
-      };
     });
-  
+
+    // it('should create a collection image', async () => {
+    //   expect
+    //   // collections aren't working at the moment, change to .collections when they are
+    //   await element(by.css('.responsive-img')).isDisplayed()
+    // });
+
+    it ('should display the correct collection image', async () => {
+      // const firstCollectionSrc = element(by.css("img[src*=Uncanny]"));
+      // expect(firstCollectionSrc.isPresent()).toBe(true);
+      expect(
+        await element(by.classname('.responsive-img')).getAttribute('src')
+      ).toContain(
+        'Uncanny'
+      );
+    });
+});
