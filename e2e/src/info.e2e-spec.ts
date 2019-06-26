bbc3eff20fb612ec73e1b2a2b2ae4297c0367dd2
Paulene Crook <pg.girl@gmail.com>
2019-06-26 15:16:53 +1200

    Start of info tests

---
+++ e2e/src/info.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { AppPage } from './app.po';
+import { browser, by, element, logging } from 'protractor';
+
+describe('Info', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it(
+    'should open the info page from the menu\n' +
+    'should display the credits information',
+  async () => {
+    await element(by.css('.btn-floating.btn-large.red')).click();
+    await element(by.css('.btn-floating.blue')).click();
+
+    expect(
+      await  element(by.css('modal-content')).element(by.tagName('h4'))
+    ).toContain(
+      'Info'
+    );
+
+    // Close the info box
+    await element(by.css('.modal-close.waves-effect.waves-green.btn-flat')).click();
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
