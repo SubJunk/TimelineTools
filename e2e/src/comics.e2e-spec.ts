edd9363e42d36c141243d4e3c965eac438bba489
Paulene Crook <pg.girl@gmail.com>
2019-07-15 16:02:44 +1200

    cleanup

--- e2e/src/comics.e2e-spec.ts
+++ e2e/src/comics.e2e-spec.ts
@@ -53,11 +53,7 @@ describe('Comics', () => {
         expect(tooltip.getText()).toContain('Mutant Mayhem');
       });
   });
-    // expect(
-    //   await element(by.css('.next-collection-title')).getText()
-    // ).toContain(
-    //   'Mutant Mayhem'
-    // );
+
     await element(by.css('.button-next-collection')).click();
 
     browser.actions().mouseMove(element(by.css('.button-next-collection'))).perform().then(() => {
@@ -71,23 +67,6 @@ describe('Comics', () => {
         expect(tooltip.getText()).toContain('X-Men: First Class - Band of Brothers');
       });
   });
-    // expect(
-    //   await element(by.css('.next-collection-title')).getText()
-    // ).toContain(
-    //   'X-Men: First Class - Band of Brothers'
-    // );
-
-
-  // });
-    // await browser.actions().mouseMove(element(by.css('.button-next-comic'))).perform().then(function() {
-    //   browser.wait(EC.textToBePresentInElement($('.next-comic-title'), 'X-Men: First Class'), 5000);
-    // }).then(function() {
-    //   expect(
-    //     element(by.css('.next-comic-title')).getText()
-    //   ).toContain(
-    //     'X-Men: First Class #2'
-    //   );
-    // });
 
     browser.actions().mouseMove(element(by.css('.button-next-comic'))).perform().then(() => {
       // perform another mousemove to try and trigger tooltip
@@ -100,10 +79,6 @@ describe('Comics', () => {
         expect(tooltip.getText()).toContain('X-Men: First Class #2');
       });
   });
-   // await browser.wait(EC.textToBePresentInElement($('.next-comic-title'), 'X-Men: First Class'), 10000);
-    // Waits for the element to contain the text
-
-
 
     // navigate forward and check the text updates
     await element(by.css('.button-next-comic')).click();
