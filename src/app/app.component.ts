aaf6483b350ee7bd87d6a6de192ad8857021c3bf
SubJunk <SubJunk@users.noreply.github.com>
2019-08-01 03:54:11 +1200

    Angular material (#197)
    * WIP Convert to Angular Material
    
    * More progress
    
    * bump
    
    * Fixed search display
    
    * Fixed tests
    
    * Cover zooming
    
    * button to anchor
    
    * Lint and compiler fixes
    
    * Cleanup and fixup
    
    * Test stability and cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+import $ from 'jquery';
 import _ from 'lodash';
-import M from 'materialize-css';
-import { Component, Injectable, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit, ViewChild } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
 import { Md5 } from 'ts-md5/dist/md5';
 
 import { apiKeyPublic, apiKeyPrivate } from './config';
@@ -20,6 +21,7 @@ import {
   SeriesVolumeLabel,
 } from './models';
 import { ActivatedRoute, Router } from '@angular/router';
+import { InfoModalComponent } from './info-modal/info-modal.component';
 
 // The padding applied to the left, right, and bottom of the body
 const BODY_PADDING_TOP = 80;
@@ -49,6 +51,7 @@ const COMPLETE_COLOR_WHEEL_DEGREES = 360; // 360 degrees in colour wheel
 @Injectable()
 export class AppComponent implements OnInit {
   constructor(
+    public dialog: MatDialog,
     private http: HttpClient,
     private route: ActivatedRoute,
     private router: Router,
@@ -66,7 +69,6 @@ export class AppComponent implements OnInit {
   // An array of objects that contain search results for comics and collections
   public itemsToSearch = [];
 
-  infoModalInstance: M.Modal;
   bodyStyles = {
     'height.px': null,
     padding: BODY_PADDING_TOP + 'px ' + BODY_PADDING + 'px ' + BODY_PADDING + 'px ' + BODY_PADDING + 'px',
@@ -85,6 +87,7 @@ export class AppComponent implements OnInit {
   globalVerticalPositionCounter = 0;
   seriesVolumeLabels: Array<SeriesVolumeLabel> = [];
 
+  expandedComic: Comic;
   expandedComicId: string;
   expandedCollectionId: string;
   expandedCollection: Collection;
@@ -345,9 +348,9 @@ export class AppComponent implements OnInit {
       return seriesVolume.id === currentComic.seriesVolumeId;
     });
 
-    const expandedComic = _.find(this.comics, ['id', this.expandedComicId]);
+    this.expandedComic = _.find(this.comics, ['id', this.expandedComicId]);
     if (this.expandedSeriesVolume.marvelId) {
-      this.setAPIComicData(expandedComic, this.expandedSeriesVolume.marvelId);
+      this.setAPIComicData(this.expandedComic, this.expandedSeriesVolume.marvelId);
     } else {
       this.getAPISeriesVolume(this.expandedSeriesVolume)
         .subscribe(
@@ -358,7 +361,7 @@ export class AppComponent implements OnInit {
 
             const firstResult: MarvelAPISeriesResponseResult = _.first(response.data.results);
             this.expandedSeriesVolume.marvelId = firstResult.id;
-            this.setAPIComicData(expandedComic, this.expandedSeriesVolume.marvelId);
+            this.setAPIComicData(this.expandedComic, this.expandedSeriesVolume.marvelId);
           },
           (err) => {
             throw new Error(err);
@@ -472,7 +475,7 @@ export class AppComponent implements OnInit {
 
       // Initialize the comic values
       comic.containerStyles = { 'left.px': null, 'top.px': null, 'width.px': null };
-      comic.classes = { stickyBottom: false, stickyLeft: false, stickyRight: false, stickyTop: false };
+      comic.classes = { fullScreen: false, stickyBottom: false, stickyLeft: false, stickyRight: false, stickyTop: false };
       comic.styles = { background: null, color: null, 'marginLeft.px': null, 'marginTop.px': null};
 
       // Horizontal positioning
@@ -799,14 +802,7 @@ export class AppComponent implements OnInit {
       $('body').height(this.bodyStyles['height.px']);
       $('body').css('padding', this.bodyStyles.padding);
 
-      // Init floating menu on the right
-      // $('.fixed-action-btn').floatingActionButton({direction: 'left'});
       const elems = document.querySelectorAll('.fixed-action-btn');
-      const instances = M.FloatingActionButton.init(elems, {direction: 'left'});
-
-      // Init "Info & Credits" modal
-      const infoModal = $('#info');
-      this.infoModalInstance = _.first(M.Modal.init(infoModal));
 
       this.useGetParameters();
 
@@ -1068,7 +1064,7 @@ export class AppComponent implements OnInit {
 
       // Instruct Materialize-CSS to make the expanded cover fullscreen on click
       const elems = document.querySelectorAll('.materialboxed');
-      const instances = M.Materialbox.init(elems);
+      // const instances = M.Materialbox.init(elems);
 
       if (this.doSpeedProfile) {
         const endTime = new Date().getTime();
@@ -1095,11 +1091,11 @@ export class AppComponent implements OnInit {
   }
 
   toggleInfoModal = () => {
-    if (this.infoModalInstance.isOpen) {
-      this.infoModalInstance.close();
-    } else {
-      this.infoModalInstance.open();
-    }
+    this.dialog.open(InfoModalComponent);
+  }
+
+  toggleFullscreen = () => {
+    this.expandedComic.classes.fullScreen = !this.expandedComic.classes.fullScreen;
   }
 
   toggleShowCollections = (forcedState?: string) => {
