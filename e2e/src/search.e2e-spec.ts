9faedfca63bb7f7e539e883b14de8f63c0548ee6
SubJunk <SubJunk@users.noreply.github.com>
2019-08-10 20:01:47 +1200

    Fixed volumes not being searchable (#213)
    * Fixed volumes not being searchable
    
    Closes #211
    
    * Display all SeriesVolume titles with Vol and number for search
    
    * Put the space back
    
    * Fix test
    
    * fix copy paste error

--- e2e/src/search.e2e-spec.ts
+++ e2e/src/search.e2e-spec.ts
@@ -16,11 +16,11 @@ describe('Search', () => {
     'should display the search div\n' +
     'should display correct search results',
   async () => {
-    await element(by.tagName('input')).sendKeys('Uncanny X-Men #100');
+    await element(by.tagName('input')).sendKeys('Uncanny X-Men Vol. 1 #100');
     expect(
       await element(by.css('.search-result')).getText()
     ).toContain(
-      'Uncanny X-Men #100'
+      'Uncanny X-Men Vol. 1 #100'
     );
   });
 
