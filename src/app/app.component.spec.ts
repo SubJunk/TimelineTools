7db9937077036a3dec825e97523cb4082a9ef424
SubJunk <SubJunk@users.noreply.github.com>
2020-12-15 14:16:28 +1300

    angular 11 (#509)
    * angular 11
    
    * bumps
    
    * Fixed dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ApiInteractions } from './api-interactions';
 import { MatDialog } from '@angular/material/dialog';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
