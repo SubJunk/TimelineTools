e655ed320cb64c93f5fe5acaa9910bc08320b967
SubJunk <subjunk@gmail.com>
2024-01-16 17:24:51 +1300

    Fail fast

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   globalSetup: require.resolve('./tests/global-setup'),
+  maxFailures: 1,
   // uncomment this to see the browser
   // use: {
   //   headless: false,
