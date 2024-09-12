a10afa2de5302f07c5afe28e83a757023434bda9
SubJunk <SubJunk@users.noreply.github.com>
2024-09-12 13:27:49 +1200

    ng18 (#1263)
    * Update yarn
    
    * temp
    
    * temp 2
    
    * ng update @angular/material@18
    
    * ng update @angular/material@18
    
    * Specify Node 20
    
    * Update other deps
    
    * Set Node version with nvmrc
    
    * Material Design 3
    
    * Changes:
    
    - Refactored tooltips from custom impl to AM, removed custom impl
    - Use system preference dark/light mode
    - Generated and added AM theme instead of using a default one
    - Customised some colors to get nicer background colors
    - Use SASS instead of LESS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltip } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -17,25 +18,21 @@ import { FilterPipe } from './filter.pipe';
 import { LimitToPipe } from './limitTo.pipe';
 import { ApiInteractions } from './api-interactions';
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         AppComponent,
         FilterPipe,
         LimitToPipe,
         InfoModalComponent,
     ],
-    imports: [
-        AppRoutingModule,
+    bootstrap: [AppComponent], imports: [AppRoutingModule,
         BrowserAnimationsModule,
         BrowserModule,
         FormsModule,
-        HttpClientModule,
         MatButtonModule,
         MatDialogModule,
         MatIconModule,
         MatMenuModule,
+        MatTooltip,
     ],
-    providers: [ApiInteractions],
-    bootstrap: [AppComponent]
-})
+    providers: [ApiInteractions, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
