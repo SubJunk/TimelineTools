a4fe589087eeba68b5fa684ecdd5f90b78f20be1
SubJunk <SubJunk@users.noreply.github.com>
2025-07-23 12:31:25 +1200

    Angular 19 (#1318)
    * Bump yarn to 4.9.2
    
    * Use npm
    
    * More npm
    
    * ng update @angular/core@19 @angular/cli@19
    
    * Fixes
    
    * accept node 20
    
    * ng update @angular/material@19
    
    * ng update @angular/core@20 @angular/cli@20
    
    * ng update @angular/material@20
    
    * Bump linter versions
    
    * Bump expected node version
    
    * Bump nvmrc version

--- src/app/info-modal/info-modal.component.ts
+++ src/app/info-modal/info-modal.component.ts
@@ -2,8 +2,9 @@ import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-info-modal',
-  templateUrl: 'info-modal.component.html',
+    selector: 'app-info-modal',
+    templateUrl: 'info-modal.component.html',
+    standalone: false
 })
 export class InfoModalComponent {
   constructor(
