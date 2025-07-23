fe778ae55c486d5c52f7012b7f2d2582cc99fcc9
SubJunk <subjunk@gmail.com>
2025-07-24 10:52:35 +1200

    Made it all standalone

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,7 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
     name: 'filter',
-    standalone: false
 })
 export class FilterPipe implements PipeTransform {
   transform(items: any[], searchText: string): any[] {
--- src/app/info-modal/info-modal.component.ts
+++ src/app/info-modal/info-modal.component.ts
@@ -1,16 +1,16 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
-    selector: 'app-info-modal',
-    templateUrl: 'info-modal.component.html',
-    standalone: false
+  imports: [
+    MatDialogModule,
+  ],
+  selector: 'app-info-modal',
+  templateUrl: 'info-modal.component.html',
 })
 export class InfoModalComponent {
-  constructor(
-    public dialogRef: MatDialogRef<InfoModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data,
-  ) {}
+  dialogRef = inject<MatDialogRef<InfoModalComponent>>(MatDialogRef);
+  data = inject(MAT_DIALOG_DATA);
 
   onNoClick(): void {
     this.dialogRef.close();
